114585c7c77f0cc5877d765dd26ef24a8aa31685
UgryumovAlex <ugryumov_alex@mail.ru>
2021-09-01 09:10:08 +0300

    products.jsp

--- src/main/java/ru/geekbrains/hw4/controllers/ProductsController.java
+++ src/main/java/ru/geekbrains/hw4/controllers/ProductsController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
